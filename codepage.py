bc2255452ba5703739eceb6ea6a5d66fb47ee0a1
olel <olel@olel.de>
2021-11-04 16:15:20 +0100

    Added script to dump codepage

---
+++ codepage.py
@@ -0,0 +1,29 @@
+import srp350
+
+printer = srp350.SRP350("/dev/usb/lp0")
+printer.initialize_printer()
+
+printer.emphasize_mode(1)
+printer.println("Codepage dump using srp350.py\n")
+printer.println("  0 1 2 3 4 5 6 7 8 9 A B C D E F")
+printer.emphasize_mode(0)
+
+for i in range(0x00, 0x10):
+    printer.emphasize_mode(1)
+    printer._handle_payload([0x0a, ord(hex(i)[-1].upper()), 0x20])
+    printer.emphasize_mode(0)
+    output = []
+    for j in range(0x00, 0x10):
+
+        chr = (i << 4) | j
+        if (chr < 0x20): chr = ord(".")
+
+        output.extend([chr, ord(" ")])
+        printer._handle_payload(output)
+        output = []
+
+printer.println("   ")
+
+printer.cut_paper(srp350.CUT_MODE_FEED_AND_CUT, 40)
+printer.send()
+printer.close()
